4329571927dedf45b493a103ab136f079187f7b9
bdbrwr <bart@bdbrwr.nl>
2025-05-23 15:01:51 +0200

    Align with boot.dev solution, repl funcionality in repl.go as startRepl()

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input_string := scanner.Text()
-		cleaned_string := cleanInput(input_string)
-		if len(cleaned_string) == 0 {
-			continue
-		}
-		command := cleaned_string[0]
-		fmt.Printf("Your command was: %s \n", command)
-	}
-
+	startRepl()
 }
