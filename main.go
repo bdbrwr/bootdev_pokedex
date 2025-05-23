74f33b2ed8f744633f96d8777be153ae362c2afe
bdbrwr <bart@bdbrwr.nl>
2025-05-23 14:58:10 +0200

    Initial REPL functionality

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input_string := scanner.Text()
+		cleaned_string := cleanInput(input_string)
+		if len(cleaned_string) == 0 {
+			continue
+		}
+		command := cleaned_string[0]
+		fmt.Printf("Your command was: %s \n", command)
+	}
+
 }
