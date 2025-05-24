8f40ead1eeb284655f2e342a35bdf89077f79f89
bdbrwr <bart@bdbrwr.nl>
2025-05-24 17:43:46 +0200

    Can catch Pokemon now

--- repl.go
+++ repl.go
@@ -13,6 +13,7 @@ type config struct {
 	pokeapiClient    pokeapi.Client
 	nextLocationsURL *string
 	prevLocationsURL *string
+	caughtPokemon    map[string]pokeapi.Pokemon
 }
 
 func startRepl(cfg *config) {
@@ -64,6 +65,11 @@ func getCommands() map[string]cliCommand {
 			name:        "help",
 			description: "Displays the help message, explaining the Usuage",
 			callback:    commandHelp,
+		},
+		"catch": {
+			name:        "catch <pokemon_name>",
+			description: "Attempt to catch a pokemon",
+			callback:    commandCatch,
 		},
 		"explore": {
 			name:        "explore <location_name>",
